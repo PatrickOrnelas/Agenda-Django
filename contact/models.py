7233660bf4f5bb26b20df71108b6fd49733f196c
Patrick Regula Ornelas <patrickornelas06@gmail.com>
2025-10-09 10:46:29 -0300

    Refazendo a cagada do banco de dados

--- contact/models.py
+++ contact/models.py
@@ -15,3 +15,27 @@ DEPOIS:
     owner(foreign key),
 
 """
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Contacts(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
