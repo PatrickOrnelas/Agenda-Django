8ec47abbd25617f2c01917862e25bfdb9d95cacf
Patrick Regula Ornelas <patrickornelas06@gmail.com>
2025-10-10 09:13:38 -0300

    Atualizando o CSS e HTML
    CSS foi atualizado e mudando o HTML juntamente com o views.py

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from contact.models import Contacts
 
 # Create your views here.
 
 def index(request):
+    contacts = Contacts.objects.all()
+
+    context = {
+        "contacts" : contacts,
+    }
     return render(
         request,
         'contact/index.html',
+        context = context
     )
