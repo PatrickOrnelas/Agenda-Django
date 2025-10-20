217965e6c5c309cc3736949c501dfc2d803f312a
Patrick Regula Ornelas <patrickornelas06@gmail.com>
2025-10-20 10:41:19 -0300

    Mensagens de erro
    Fazendo validações com if e mostrando as mensagens de erro

--- contact/forms.py
+++ contact/forms.py
@@ -45,21 +45,28 @@ class ContactsForm(forms.ModelForm):
         }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
+        msg = ValidationError(
+                    'Primeiro nome não pode ser igual ao segundo.',
+                    code='invalid'
+                )
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+        if first_name == last_name:
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError('Não digite ABC neste campo.',
+                code='invalid')
+            )
+        return first_name
