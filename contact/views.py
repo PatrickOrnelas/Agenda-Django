53e4b100073d34174a3ab089f4a6a141406e8a76
Patrick Regula Ornelas <patrickornelas06@gmail.com>
2025-11-04 15:00:43 -0300

    Update
    Foi adicionado 2 botões na pagina de contato, um para editar e outro para deletar, foi feita uma logica por trás do deletar onde precisa de uma confirmação, após clicar em deletar o botão atualiza com uma pergunta confirmando se realmente quer deletar, clicando novamente,  o contato é deletado  e o usuário é redirecionado para a home do site.

--- contact/views.py
+++ contact/views.py
@@ -132,3 +132,16 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contacts, pk=contact_id, show=True)
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(request, 'contact/contact.html', {
+        'contact': contact,
+        'confirmation' : confirmation, 
+    })
