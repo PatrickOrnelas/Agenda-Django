d5f1fde3e2e74b799ce0e27bf14322aa95182d85
Patrick Regula Ornelas <patrickornelas06@gmail.com>
2025-11-05 14:06:01 -0300

    Update
    Fazendo as validações de cadastro de um usuário. o usuário é salvo no banco caso o formulário seja valido, ou seja, nenhum erro e foi inserido uma validação de email onde garante que um email não pode ser usado em mais de 1 cadastro

--- contact/forms.py
+++ contact/forms.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactsForm(forms.ModelForm):
 
@@ -108,4 +109,21 @@ class ContactsForm(forms.ModelForm):
         return first_name
     
 class RegisterForm(UserCreationForm):
-    pass
+    first_name = forms.CharField(
+        required=True,
+    )
+    last_name = forms.CharField(
+        required=True,
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', ValidationError('Email já cadastrado.', code='invalid'))
+        return email
